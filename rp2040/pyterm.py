a631cec15900c0c4652097b3e7939bac2a27f19b
mareksaw-cg <82727321+mareksaw-cg@users.noreply.github.com>
2021-06-23 13:01:15 +0200

    Update pyterm.py

--- rp2040/pyterm.py
+++ rp2040/pyterm.py
@@ -1,31 +1,54 @@
 from machine import Pin, Timer, UART
-from time import sleep 
+from time import sleep, ticks_ms
 
-uart = UART(0, 57600, tx=Pin(16), rx=Pin(17))
+uart = UART(1, 57600, tx=Pin(4), rx=Pin(5))
+reset = Pin(3, Pin.OUT, value=0)
 tim = Timer()
 cmd = ''
+
+def decode(bytestream):
+    try:
+        rxdata = bytestream.decode('ascii').strip()
+        if rxdata:
+            print(rxdata[(rxdata.find(cmd) + len(cmd)):].strip())
+    except:
+        print('Error')
+
+def send(cmd, timeout=1000):
+    clear_buf()
+    uart.write(cmd + "\r\n")
+    t1 = ticks_ms()
+    while not uart.any():
+        t2 = ticks_ms()
+        if t2 - t1 > timeout:
+            break
+    if uart.any():
+        rxdata = uart.read()
+        return rxdata
+    else:
+        return b''
+    
+def clear_buf():
+    if uart.any():
+        rxdata = uart.read()
 
 def tick(timer):
     global cmd
     if cmd:
-        uart.write(cmd + '\r\n')
-        sleep(0.1)
-        
-    if uart.any():
-        try:
-            rxdata = uart.read().decode('ascii').strip()
-            if rxdata:
-                print(rxdata[(rxdata.find(cmd) + len(cmd)):].strip())
-        except:
-            print('Error')
-    cmd = '' 
+        tim.deinit()
+        decode(send(cmd))
+        cmd = ''
+        tim.init(freq=20, mode=Timer.PERIODIC, callback=tick)
+    else:
+        if uart.any():
+            decode(uart.read())
 
 tim.init(freq=20, mode=Timer.PERIODIC, callback=tick)
 
 rxdata = uart.read()
 rxdata = ''
 while True:
-    cmd = input()
+    cmd = input('> ')
     if cmd == 'quit':
         break
     sleep(0.1)
